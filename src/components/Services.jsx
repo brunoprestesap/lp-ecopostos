ceb3f7e6b79d7dd7c825f1faf6bc4934b6e9e872
Bruno Prestes <brunoasprestes@gmail.com>
2023-03-24 12:05:32 -0300

    Criacao da secao de servicos

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,20 +3,48 @@ import { FaFilter, FaWhatsapp } from "react-icons/fa";
 
 const Services = () => {
   return (
-    <div className="p-5 bg-yellow-50 flex flex-col items-center w-full">
+    <div className="p-5 bg-yellow-50 flex flex-col items-center w-full gap-5">
       <h2 className="text-center font-bold text-4xl text-blue-800 mb-5">
         Nossos Serviços
       </h2>
 
-      <div className="p-5 w-5/6 bg-blue-800 rounded-2xl flex flex-col justify-center items-center">
+      <div className="p-5 w-5/6 bg-yellow-500 rounded-2xl flex flex-col justify-center items-center">
         <FaFilter size={50} color="FFFFFF" />
         <h2 className="text-xl font-bold text-white">Troca de óleo</h2>
         <p className="text-sm text-justify mt-3 text-white">
-          Agende agora a troca de óleo do seu carro em poucos minutos -
-          economize tempo e dinheiro com nossos serviços profissionais!
+          Com nossos serviços profissionais e eficientes, você garante o bom funcionamento do motor e evita problemas mais graves no futuro. 
+          Agende agora e tenha a tranquilidade de estar cuidando bem do seu veículo.
         </p>
         <button className="bg-white p-2 rounded-lg mt-3 flex items-center gap-3">
-          <FaWhatsapp size={20} color='1E40AF' />
+          <FaWhatsapp size={20} />
+          <p className="text-black text-base antialiased">Agende agora</p>
+        </button>
+      </div>
+
+      {/* Servico de alinhamento */}
+      <div className="p-5 w-5/6 bg-green-800 rounded-2xl flex flex-col justify-center items-center">
+        <FaFilter size={50} color="FFFFFF" />
+        <h2 className="text-xl font-bold text-white">Alinhamento</h2>
+        <p className="text-sm text-justify mt-3 text-white">
+          Deixe seu carro pronto para encarar o dia-a-dia com nossos serviços de alinhamento de última geração!
+          Agende agora e sinta a diferença no desempenho do seu veículo
+        </p>
+        <button className="bg-white p-2 rounded-lg mt-3 flex items-center gap-3">
+          <FaWhatsapp size={20} />
+          <p className="text-black text-base antialiased">Agende agora</p>
+        </button>
+      </div>
+
+      {/* Balanceamento */}
+      <div className="p-5 w-5/6 bg-blue-800 rounded-2xl flex flex-col justify-center items-center">
+        <FaFilter size={50} color="FFFFFF" />
+        <h2 className="text-xl font-bold text-white">Balanceamento</h2>
+        <p className="text-sm text-justify mt-3 text-white">
+          Sinta a diferença no conforto e na dirigibilidade do seu carro com nossos serviços de balanceamento de rodas! 
+          Agende agora e tenha a certeza de estar cuidando bem do seu veículo.
+        </p>
+        <button className="bg-white p-2 rounded-lg mt-3 flex items-center gap-3">
+          <FaWhatsapp size={20} />
           <p className="text-black text-base antialiased">Agende agora</p>
         </button>
       </div>
