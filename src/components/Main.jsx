244c6f2021a4ad22d1d1fa8c53617fbbf8e67894
Bruno Prestes <brunoasprestes@gmail.com>
2023-03-24 10:37:23 -0300

    Ajustes no texto

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,7 +13,7 @@ const Main = () => {
         </div>
         <h1 className='text-blue-900 font-bold text-6xl text-center antialiased'>Ecopostos</h1>
         <h2 className='text-center font-bold text-green-900 text-xl my-1'>Combustíveis e troca de óleo</h2>
-        <h2 className='text-2xl text-blue-900 text-center font-bold my-16 antialiased'>Abasteça com o combustível mais confiável do estado ou agende agora a troca de óleo e mantenha seu carro em perfeitas condições - deixe-nos cuidar disso para você!</h2>
+        <h2 className='text-2xl text-blue-900 text-center font-bold my-16 antialiased'>Abasteça com o combustível mais confiável do estado ou agende agora a troca de óleo e mantenha seu carro em perfeitas condições. Deixe-nos cuidar disso para você!</h2>
 
         <div className='flex flex-col justify-center gap-5'>
             <button className='bg-blue-900 shadow-md shadow-slate-700 rounded-xl p-5'>
