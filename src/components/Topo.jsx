efb741177bfd90e7a804ad771bb134f5423b5782
Bruno Prestes <brunoasprestes@gmail.com>
2023-03-24 14:16:03 -0300

    ajustes no topo da pagina

--- src/components/Topo.jsx
+++ src/components/Topo.jsx
@@ -6,32 +6,32 @@ import { FaGasPump, FaWhatsapp } from "react-icons/fa";
 
 const Topo = () => {
   return (
-    <div className="p-5 w-full h-screen bg-gradient-to-b from-[#F6C234] via-yellow-500 to-[#275336] flex flex-col items-center">
-      <div className="flex justify-center w-full">
-        <img
-          className="rounded-xl w-72 shadow-xl shadow-blue-500/50"
-          src={logo}
-          alt="logo"
-        />
-      </div>
+    <div id="topo" className="relative">
+      <img
+        src={bg}
+        alt="fundo do topo"
+        className="w-full h-screen object-cover"
+      />
 
-      <div className="flex w-full items-center justify-center flex-col-reverse lg:flex-row my-12">
-        <div className="w-full">
-          <h2 className="text-2xl text-blue-900 text-center lg:text-5xl lg:text-left font-bold my-8 lg:my-16 antialiased">
-            Agende agora a troca de óleo do seu carro com uma equipe
-            especializada e mantenha seu carro em perfeitas condições. Deixe-nos
-            cuidar disso para você!
-          </h2>
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/30">
+        <div className="flex aboslute top-0 left-0 justify-center w-full">
+          <img
+            className="rounded-xl w-72 shadow-xl shadow-white/50 m-5"
+            src={logo}
+            alt="logo"
+          />
+        </div>
 
-          <div className="flex flex-col lg:flex-row justify-start gap-5">
-            <button className="bg-blue-900 shadow-md shadow-slate-700 rounded-xl p-5">
-              <div className="flex justify-center items-center gap-4">
-                <FaGasPump size={25} color="FFFFFF" />
-                <h2 className="text-white font-bold">
-                  Localize o Ecoposto mais pŕoximo
-                </h2>
-              </div>
-            </button>
+        <div className="flex flex-col w-full items-center justify-center p-5">
+          <div className="lg:w-3/6 backdrop-opacity-10 bg-white/30 rounded-xl p-5 shadow-xl shadow-white/20">
+            <h2 className="text-3xl text-blue-900 text-center lg:text-5xl font-bold my-8 lg:my-16 antialiased">
+              Agende agora a troca de óleo do seu carro com uma equipe
+              especializada e mantenha seu carro em perfeitas condições.
+              Deixe-nos cuidar disso para você!
+            </h2>
+          </div>
+
+          <div className="flex flex-col lg:flex-row justify-start gap-5 absolute bottom-10 left-15">
             <button className="bg-blue-900 shadow-md shadow-slate-700 rounded-xl p-5">
               <div className="flex justify-center items-center gap-4">
                 <FaWhatsapp size={25} color="FFFFFF" />
@@ -40,14 +40,15 @@ const Topo = () => {
                 </h2>
               </div>
             </button>
+            <button className="bg-blue-900 shadow-md shadow-slate-700 rounded-xl p-5">
+              <div className="flex justify-center items-center gap-4">
+                <FaGasPump size={25} color="FFFFFF" />
+                <h2 className="text-white font-bold">
+                  Localize o Ecoposto mais pŕoximo
+                </h2>
+              </div>
+            </button>
           </div>
-        </div>
-        <div className="w-full rounded-xl flex items-center justify-center lg:justify-end">
-          <img
-            src={bg}
-            className="w-5/6 rounded-3xl shadow-xl shadow-black/50"
-            alt="foto trocando o oleo"
-          />
         </div>
       </div>
     </div>
