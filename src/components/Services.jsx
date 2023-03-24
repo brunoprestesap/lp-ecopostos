519cfefbf2c789ed67f4668da7f1c7036a2f95cc
Bruno Prestes <brunoasprestes@gmail.com>
2023-03-24 12:53:32 -0300

    ajustes no layout da pagina

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,53 +1,59 @@
 import React from "react";
 import { FaFilter, FaWhatsapp, FaCar } from "react-icons/fa";
-import { MdOutlineCarRepair } from 'react-icons/md'
+import { MdOutlineCarRepair } from "react-icons/md";
 
 const Services = () => {
   return (
-    <div className="p-5 bg-yellow-50 flex flex-col items-center w-full gap-5">
+    <div className="p-5 bg-yellow-50 flex flex-col items-center w-full">
       <h2 className="text-center font-bold text-4xl text-blue-800 mb-5">
         Nossos Serviços
       </h2>
 
-      <div className="p-5 w-5/6 bg-yellow-500 rounded-2xl flex flex-col justify-center items-center shadow-lg shadow-slate-600">
-        <FaFilter size={50} color="FFFFFF" />
-        <h2 className="text-2xl font-bold text-white">Troca de óleo</h2>
-        <p className="text-sm text-justify mt-3 text-white">
-          Com nossos serviços profissionais e eficientes, você garante o bom funcionamento do motor e evita problemas mais graves no futuro. 
-          Agende agora e tenha a tranquilidade de estar cuidando bem do seu veículo.
-        </p>
-        <button className="bg-white p-2 rounded-lg mt-3 flex items-center gap-3">
-          <FaWhatsapp size={20} />
-          <p className="text-black text-base antialiased">Agende agora</p>
-        </button>
-      </div>
+      <div className="lg:w-5/6 grid grid-cols-1 lg:grid-cols-3 justify-items-center gap-5">
+        <div className="p-5 w-5/6 bg-yellow-500 rounded-2xl flex flex-col justify-center items-center shadow-lg shadow-slate-600">
+          <FaFilter size={50} color="FFFFFF" />
+          <h2 className="text-2xl font-bold text-white">Troca de óleo</h2>
+          <p className="text-sm text-justify mt-3 text-white">
+            Com nossos serviços profissionais e eficientes, você garante o bom
+            funcionamento do motor e evita problemas mais graves no futuro.
+            Agende agora e tenha a tranquilidade de estar cuidando bem do seu
+            veículo.
+          </p>
+          <button className="bg-white p-2 rounded-lg mt-3 flex items-center gap-3">
+            <FaWhatsapp size={20} />
+            <p className="text-black text-base antialiased">Agende agora</p>
+          </button>
+        </div>
 
-      {/* Servico de alinhamento */}
-      <div className="p-5 w-5/6 bg-green-800 rounded-2xl flex flex-col justify-center items-center shadow-lg shadow-slate-600">
-        <MdOutlineCarRepair size={50} color="FFFFFF" />
-        <h2 className="text-2xl font-bold text-white">Alinhamento</h2>
-        <p className="text-sm text-justify mt-3 text-white">
-          Deixe seu carro pronto para encarar o dia-a-dia com nossos serviços de alinhamento de última geração!
-          Agende agora e sinta a diferença no desempenho do seu veículo
-        </p>
-        <button className="bg-white p-2 rounded-lg mt-3 flex items-center gap-3">
-          <FaWhatsapp size={20} />
-          <p className="text-black text-base antialiased">Agende agora</p>
-        </button>
-      </div>
+        {/* Servico de alinhamento */}
+        <div className="p-5 w-5/6 bg-green-800 rounded-2xl flex flex-col justify-center items-center shadow-lg shadow-slate-600">
+          <MdOutlineCarRepair size={50} color="FFFFFF" />
+          <h2 className="text-2xl font-bold text-white">Alinhamento</h2>
+          <p className="text-sm text-justify mt-3 text-white">
+            Deixe seu carro pronto para encarar o dia-a-dia com nossos serviços
+            de alinhamento de última geração! Agende agora e sinta a diferença
+            no desempenho do seu veículo
+          </p>
+          <button className="bg-white p-2 rounded-lg mt-3 flex items-center gap-3">
+            <FaWhatsapp size={20} />
+            <p className="text-black text-base antialiased">Agende agora</p>
+          </button>
+        </div>
 
-      {/* Balanceamento */}
-      <div className="p-5 w-5/6 bg-blue-800 rounded-2xl flex flex-col justify-center items-center shadow-lg shadow-slate-600">
-        <FaCar size={50} color="FFFFFF" />
-        <h2 className="text-2xl font-bold text-white">Balanceamento</h2>
-        <p className="text-sm text-justify mt-3 text-white">
-          Sinta a diferença no conforto e na dirigibilidade do seu carro com nossos serviços de balanceamento de rodas! 
-          Agende agora e tenha a certeza de estar cuidando bem do seu veículo.
-        </p>
-        <button className="bg-white p-2 rounded-lg mt-3 flex items-center gap-3">
-          <FaWhatsapp size={20} />
-          <p className="text-black text-base antialiased">Agende agora</p>
-        </button>
+        {/* Balanceamento */}
+        <div className="p-5 w-5/6 bg-blue-800 rounded-2xl flex flex-col justify-center items-center shadow-lg shadow-slate-600">
+          <FaCar size={50} color="FFFFFF" />
+          <h2 className="text-2xl font-bold text-white">Balanceamento</h2>
+          <p className="text-sm text-justify mt-3 text-white">
+            Sinta a diferença no conforto e na dirigibilidade do seu carro com
+            nossos serviços de balanceamento de rodas! Agende agora e tenha a
+            certeza de estar cuidando bem do seu veículo.
+          </p>
+          <button className="bg-white p-2 rounded-lg mt-3 flex items-center gap-3">
+            <FaWhatsapp size={20} />
+            <p className="text-black text-base antialiased">Agende agora</p>
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/Topo.jsx
+++ src/components/Topo.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from "../assets/logo.jpeg";
+import bg from "../assets/background.jpg";
 
 import { FaGasPump, FaWhatsapp } from "react-icons/fa";
 
 const Topo = () => {
   return (
-    <div className="p-5 w-full h-screen bg-gradient-to-b from-[#F6C234] via-yellow-500 to-[#275336]">
+    <div className="p-5 w-full h-screen bg-gradient-to-b from-[#F6C234] via-yellow-500 to-[#275336] flex flex-col items-center">
       <div className="flex justify-center w-full">
         <img
           className="rounded-xl w-72 shadow-xl shadow-blue-500/50"
@@ -14,28 +15,40 @@ const Topo = () => {
         />
       </div>
 
-      <h2 className="text-3xl text-blue-900 text-center font-bold my-16 antialiased">
-        Agende agora a troca de óleo do seu carro com uma equipe especializada e mantenha seu carro em perfeitas
-        condições. Deixe-nos cuidar disso para você!
-      </h2>
+      <div className="flex w-full items-center justify-center flex-col-reverse lg:flex-row my-12">
+        <div className="w-full">
+          <h2 className="text-3xl text-blue-900 text-center lg:text-5xl lg:text-left font-bold my-8 lg:my-16 antialiased">
+            Agende agora a troca de óleo do seu carro com uma equipe
+            especializada e mantenha seu carro em perfeitas condições. Deixe-nos
+            cuidar disso para você!
+          </h2>
 
-      <div className="flex flex-col justify-center gap-5">
-        <button className="bg-blue-900 shadow-md shadow-slate-700 rounded-xl p-5">
-          <div className="flex justify-center items-center gap-4">
-            <FaGasPump size={25} color="FFFFFF" />
-            <h2 className="text-white font-bold">
-              Localize o Ecoposto mais pŕoximo
-            </h2>
+          <div className="flex flex-col lg:flex-row justify-start gap-5">
+            <button className="bg-blue-900 shadow-md shadow-slate-700 rounded-xl p-5">
+              <div className="flex justify-center items-center gap-4">
+                <FaGasPump size={25} color="FFFFFF" />
+                <h2 className="text-white font-bold">
+                  Localize o Ecoposto mais pŕoximo
+                </h2>
+              </div>
+            </button>
+            <button className="bg-blue-900 shadow-md shadow-slate-700 rounded-xl p-5">
+              <div className="flex justify-center items-center gap-4">
+                <FaWhatsapp size={25} color="FFFFFF" />
+                <h2 className="text-white font-bold">
+                  Agende a sua troca de óleo
+                </h2>
+              </div>
+            </button>
           </div>
-        </button>
-        <button className="bg-blue-900 shadow-md shadow-slate-700 rounded-xl p-5">
-          <div className="flex justify-center items-center gap-4">
-            <FaWhatsapp size={25} color="FFFFFF" />
-            <h2 className="text-white font-bold">
-              Agende a sua troca de óleo
-            </h2>
-          </div>
-        </button>
+        </div>
+        <div className="w-full rounded-xl flex items-center justify-center lg:justify-end">
+          <img
+            src={bg}
+            className="w-5/6 rounded-3xl shadow-xl shadow-black/50"
+            alt="foto trocando o oleo"
+          />
+        </div>
       </div>
     </div>
   );
